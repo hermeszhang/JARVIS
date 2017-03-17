17233a7635f81c692f626ece8d91a96f5ffbcc95
kochjohannes <johannes.koch@live.se>
2017-03-17 14:50:21 +0100

    Add files via upload

---
+++ send_audio_block/send_audio_block.ino
@@ -0,0 +1,25 @@
+const int pin = 0;
+int indx = 0;
+int block[320];
+string sblock;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  t = micros();
+  if(t - t_old => 62){
+    block[indx] = analogRead();
+    t = t_old;
+    indx++;
+  }
+  if(indx >= 320){
+    for(int i=0; i<320; i++){
+      sblock += block[i];
+    }
+    Serial.println(sblock);
+    sblock = "";
+  }
+}
+
