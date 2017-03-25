70198d4862691ad1da10f6372e97e4b9f6c0a18a
Olle Ferling <tna12ofe@student.lu.se>
2017-03-25 13:07:41 +0100

    Added simulation files provided. Introduced file structure

--- send_audio_block/send_audio_block.ino
+++ send_audio_block/send_audio_block.ino
@@ -1,7 +1,13 @@
 const int pin = 0;
+const float midlevel = 3.25;
+const float quantlevel = 1024.0;
+const int maxlevel = 5;
 int indx = 0;
-int block[320];
-string sblock;
+float block[320] = {0.0};
+int t = 0;
+int t_old = 0;
+String sblock;
+
 
 void setup() {
   Serial.begin(115200);
@@ -9,17 +15,21 @@ void setup() {
 
 void loop() {
   t = micros();
-  if(t - t_old => 62){
-    block[indx] = analogRead();
-    t = t_old;
+  if((t - t_old) >= 62){
+    block[indx] = analogRead(pin);//((analogRead(pin)/quantlevel)*maxlevel - midlevel); //Convert signal to valid sound signal
+    t_old = t;
     indx++;
   }
   if(indx >= 320){
     for(int i=0; i<320; i++){
-      sblock += block[i];
+      //sblock += block[i];
+      //sblock += ", ";
+      Serial.print(block[i]);
+      Serial.print(", ");
     }
-    Serial.println(sblock);
+    Serial.println();
     sblock = "";
+    indx = 0;
   }
 }
 
